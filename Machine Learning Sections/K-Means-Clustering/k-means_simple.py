a3ef6e22693f4d379f0a2212c1b0070e190e17b2
Khanh Nguyen <nvtk@yahoo.com>
2018-02-26 17:16:45 -0600

    updae cells

--- Machine Learning Sections/K-Means-Clustering/k-means_simple.py
+++ Machine Learning Sections/K-Means-Clustering/k-means_simple.py
@@ -9,3 +9,30 @@ y = [2, 8, 1.8, 8, 0.6, 11]
 
 plt.scatter(x, y)
 plt.show()
+
+
+X = np.array([[1, 2],
+              [5, 8],
+              [1.5, 1.8],
+              [8, 8],
+              [1, 0.6],
+              [9, 11]])
+
+
+kmeans = KMeans(n_clusters=2)
+kmeans.fit(X)
+
+centroid = kmeans.cluster_centers_
+labels = kmeans.labels_
+
+print(centroids)
+
+colors = ["g.", "r."]
+for i in range(len(X)):
+    print ("coordinate:", X[i], "label:", labels[i])
+    plt.plt(X[i][0], X[i], [1], colors[labels[i]], markersize=10)
+
+
+plt.scatter(centroid[:, 0], centroid[:1], marker="x",
+            s=150, linewidths=5, zorder=10)
+plt.show()
